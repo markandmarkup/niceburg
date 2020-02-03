3c5dc609fa5ef897241bc6f11e23fdff73ba8466
markandmarkup <markeyles88@gmail.com>
2020-02-03 15:50:46 +0000

    Record html added, not styled

--- index.php
+++ index.php
@@ -16,6 +16,29 @@
         <h1>NiceBurg</h1>
         <h2>Burger reviews to sink your teeth into</h2>
     </header>
+
+    <div class="section-header">
+        <h3>All Reviews</h3>
+    </div>
+
+    <section class="review-container">
+        <div class="review">
+            <div class="review-content">
+                <img src="./images/smoky-bro1-725x479.jpg" />
+                <h4>Smokey 'Bro' Burger</h4>
+                <h5>Three Brothers Bristol</h5>
+                <div class="stats">
+                    <p>Visit Date: <span>01/02/2020</span></p>
+                    <p>Price: <span>£8.50</span></p>
+                    <p>Burger Patty: <span>4</span></p>
+                    <p>Toppings &amp; bun: <span>4.5</span></p>
+                    <p>Sides: <span>3</span></p>
+                    <p>Value: <span>3.5</span></p>
+                    <p>Rating: <span>3.8</span></p>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
 </body>
 </html>
