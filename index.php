51d1a26041e0962ec4d9eadce1d51e388790c0b7
markandmarkup <markeyles88@gmail.com>
2020-02-03 14:30:13 +0000

    html header, css background and basic centering

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <title>NiceBurg | Burger Reviews</title>
+</head>
+<body>
+<div class="content-column">
+    <header>
+        <img src="./images/logoburger_400.png" />
+        <h1>NiceBurg</h1>
+        <h2>Burger reviews to sink your teeth into</h2>
+    </header>
+</div>
+</body>
+</html>
